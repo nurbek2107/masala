70ef510d95cbf7c4c50ac740ffbf8562b1d1cf96
nurbek2107 <abduaxatov002@gmail.com>
2024-05-02 10:32:06 +0500

    masala
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

---
+++ main.js
@@ -0,0 +1,215 @@
+// 1-misol
+
+// function sum(x, y) {
+//     console.log(x+y)
+// }
+// sum(3, 2)
+// sum(-3, -6)
+// sum(7, 3)
+
+// 2-misol
+
+// function toSekunds(minutes) {
+//     console.log(minutes*60);
+// }
+// toSekunds(5)
+// toSekunds(3)
+// toSekunds(2)
+
+// 3-misol
+
+// function nextInteger(int) {
+//     console.log(int+1);
+// }
+// nextInteger(0)
+// nextInteger(9)
+// nextInteger(-3)
+
+// 4-misol
+
+// function uchburchakYuzi(asos, balandlik) {
+//     console.log((asos * balandlik) / 2);
+// }
+// uchburchakYuzi(3, 2)
+
+// uchburchakYuzi(7, 4)
+
+// uchburchakYuzi(10, 10)
+
+// 5-misol
+
+// function ageToDays(age) {
+//     console.log(age*365)
+// }
+// ageToDays(65)
+// ageToDays(0)
+// ageToDays(20)
+
+// 6-misol
+
+// function kub(num) {
+//     console.log(num**3)
+// }
+// kub(3)
+// kub(5)
+// kub(10)
+
+// 7-misol
+
+// function firstElement(arr) {
+//     console.log(arr[0]);
+// }
+// firstElement([1, 2, 3])
+// firstElement([80, 5, 100])
+// firstElement([-500, 0, 50])
+
+// 8-misol
+
+// function power(x, y) {
+//     console.log(x*y);
+// }
+// power(230, 10)
+// power(110, 3)
+// power(480, 20)
+
+// 9-misol
+
+// function hourToSekunds(hour) {
+//     console.log(hour*3600)
+// }
+// hourToSekunds(2)
+// hourToSekunds(10)
+// hourToSekunds(24)
+
+// 10-misol
+
+// function uchinchiTomon(tomon1, tomon2) {
+//     console.log( (tomon1 + tomon2) - 1)
+// }
+// uchinchiTomon(8, 10)
+// uchinchiTomon(5, 7)
+// uchinchiTomon(9, 2)
+
+// 11 - misol
+// function qoldiq(son1, son2) {
+//     console.log(son1 %  son2)
+// }
+// qoldiq(1, 3)
+// qoldiq(3, 4)
+// qoldiq(-9, 45)
+// qoldiq(5, 5)
+
+// 12-misol
+// function turtburchakYuzi(boyi, eni) {
+//     console.log(boyi * eni)
+// }
+// turtburchakYuzi(6, 7)
+// turtburchakYuzi(20, 10)
+// turtburchakYuzi(2, 9)
+
+//13-misol
+
+// function stringQoshish(a) {
+//     return "something " + a;
+// }
+
+// console.log(stringQoshish("is better than nothing"));
+// console.log(stringQoshish("Bob Jane"));
+// console.log(stringQoshish("something"));
+
+// 14 - misol
+
+// function kvadrat(a) {
+//     console.log(a*a)
+// }
+// kvadrat(5)
+// kvadrat(9)
+// kvadrat(100)
+
+// 15-misol
+// function noldan(raqam) {
+//     if (raqam <= 0) {
+//         console.log(true)
+//     } else {
+//         console.log(false)
+//     }
+// }
+// noldan(5)
+// noldan(0)
+// noldan(-2)
+
+// 16-misol
+
+// function ichkiBurchaklar(n) {
+//     console.log((n - 2) * 180);
+// }
+// ichkiBurchaklar(3)
+// ichkiBurchaklar(4)
+// ichkiBurchaklar(6)
+
+// 17-misol
+
+// function ochkolar(x, y) {
+//     console.log((x*2)+(y*3));
+// }
+// ochkolar(1, 1)
+// ochkolar(7, 5)
+// ochkolar(38, 8)
+
+// 18-misol
+// function nameString(params) {
+//     console.log(params += "Edabit")
+// }
+// nameString("Mubashir")
+// nameString("Matt")
+// nameString("javaScript")
+
+// 19-misol
+// function kichik100dan(x, y) {
+//     let sam = x + y
+//     if (sam>=100) {
+//         console.log(false);
+//     } else {
+//         console.log(true)
+//     }
+// }
+// kichik100dan(22, 15)
+// kichik100dan(83, 34)
+// kichik100dan(3, 77)
+
+// 20-misol
+// function printArray(a) {
+//     let arr = []
+//     for (let i = 1; i <= a; i++){
+//         arr.push(i)
+//     }
+//     console.log(arr)
+// }
+// printArray(1)
+// printArray(3)
+// printArray(6)
+
+// 21-misol
+// function oyoqlar(tovuq, qoy, sigir) {
+//     console.log((tovuq * 2) + (qoy * 4) + (sigir*4))
+// }
+// oyoqlar(2, 3, 5)
+// oyoqlar(1, 2, 3)
+// oyoqlar(5, 2, 8)
+
+// 22-misol
+// function and(a,b) {
+//     if (a && b) {
+//         console.log(true);
+//     }
+//     else {
+//         console.log(false);
+//     }
+// }
+
+// and(true, false)
+// and(true, true)
+// and(false, true)
+// and(false, false)
+
+// 23-misol
